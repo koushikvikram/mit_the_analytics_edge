425e5f93422180b5de734f752fd59349f3ac5d38
koushikvikram <koushikvikram91@gmail.com>
2017-06-10 18:27:04 -0500

    undertanding food - background info

---
+++ unit_1_intro_to_analytics/recitation_1_understanding_food.R
@@ -0,0 +1,32 @@
+# Recitation 1 - Undertanding Food: Nutritional Education with Data
+
+# Use Ctrl+Shift+C for multi line comment
+
+# Nutritional Facts about food - 
+#   Good nutrition is an important part of leading a healthy lifestyle
+#   Malnutrition can lead to obesity
+#   
+# Obesity -
+#   More than 35% of US adults are obese - 
+#     Obesity-related conditions are some of the leading causes of preventable death (heart disease, stroke, type II diabetes)
+# 
+#   Worldwide, obesity has nearly doubled since 1980
+#   
+#   65% of the world's population lives in countries where overweight and obesity kills more people than underweight
+
+# Nutrition -
+#   Good nutrition is essential for a person's overall health and well being, and is now more important than ever
+# 
+#   Hundreds of nutrition and weight-loss applications -
+#     15% of adults with cell phones use health applications on their devices
+# 
+#   These apps are powered by the USDA Food Database
+
+# USDA Food Database - 
+#   The United States Department of Agriculture distributes a database of nutritional information for over 7,000 different food items
+# 
+# Used as the foundation for most food and nutrient databases in the US
+# 
+# Includes information about all nutrients -
+#   Calories, carbs, proteins, fat, sodium, ...
+
